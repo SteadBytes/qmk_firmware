4b6d31073261169ccfc5def2ef43fca3902288f4
Ben Steadman <ben@steadbytes.com>
2022-02-03 21:09:08 +0000

    Fix space cadet shift keys
    Signed-off-by: Ben Steadman <ben@steadbytes.com>

--- keyboards/crkbd/keymaps/SteadBytes/keymap.c
+++ keyboards/crkbd/keymaps/SteadBytes/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      LCTL_T(KC_ESC), KC_A, KC_S,  KC_D,  KC_F,     KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-    LSFT_T(KC_LPRN), KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, RSFT_T(KC_RPRN),
+     KC_LSPO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M,    KC_COMM,  KC_DOT, KC_SLSH, KC_RSPC,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                               KC_LGUI, KC_SPACE, LT(_NUM, KC_TAB),    LT(_NAV, KC_ENT), KC_BSPC, KC_RALT
                                       //`--------------------------'  `--------------------------'
