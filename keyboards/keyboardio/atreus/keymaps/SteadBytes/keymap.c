92a1cac18a7d5a4eefc3681efb398d949efa4ad5
Ben Steadman <ben@steadbytes.com>
2022-04-08 10:14:16 +0100

    Re-map based off my crkbd layout
    Includes a number of changes to account for the reduced keys available, but still similar overall. Adjust layer not yet implemented.
    
    Signed-off-by: Ben Steadman <ben@steadbytes.com>

--- keyboards/keyboardio/atreus/keymaps/SteadBytes/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/SteadBytes/keymap.c
@@ -21,49 +21,68 @@ Atreus.
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _QW,  // QWERTY
-    _RS,  // RAISE
-    _LW,  // LOWER
+    _QW,   // QWERTY
+    _NUM,  // Number + symbol
+    _NAV,  // Navigation
+    // TODO: Adjust layer
 };
+
+enum custom_keycodes {
+    TMUX_LEAD = SAFE_RANGE,
+};
+
+// TODO: better ctrl position(s)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
-   *  q    w    e    r      t          ||       y      u   i  o  p
-   *  a    s    d    f      g          ||       h      j   k  l  ;
-   *  z    x    c    v      b    ~     ||  \    n      m   ,  .  /
-   *  esc  tab  gui  shift  bsp  ctrl  ||  alt  space  fn  -  '  enter
+   *  q        w          e    r    t                ||            y    u      i  o  p
+   *  a        s          d    f    g                ||            h    j      k  l  ;
+   *  z        x          c    v    b     TMUX_LEAD  ||  \         n    m      ,  .  /
+   *  shift/(  ctrl/esc  alt  gui  space  tab/L1     ||  enter/L2  bsp  altgr  -  '  shift/)
    */
   [_QW] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_GRV,  KC_BSLS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_Q,    KC_W,           KC_E,    KC_R,    KC_T,                                           KC_Y,    KC_U,     KC_I,    KC_O,    KC_P   ,
+    KC_A,    KC_S,           KC_D,    KC_F,    KC_G,                                           KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,
+    KC_Z,    KC_X,           KC_C,    KC_V,    KC_B,      TMUX_LEAD,            KC_BSLS,       KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,
+    KC_LSPO, LCTL_T(KC_ESC), KC_LALT, KC_LGUI, KC_SPACE,  LT(_NUM, KC_TAB),    LT(_NAV, KC_ENT), KC_BSPC, KC_RALT , KC_MINS, KC_QUOT, KC_RSPC
   ),
 
   /*
-   *  !       @      up    {      }           ||       pgup   7   8  9  *
-   *  #     left    down   right  $           ||       pgdn   4   5  6  +
-   *  [       ]     (      )      &           ||         `    1   2  3  \
-   * lower  insert  super  shift  bksp  ctrl  ||  alt  space  fn  .  0  =
+   *  !        @         #    $    #              ||            ^    &      *  -  +
+   *  1        2         3    4    5              ||            6    7      8  9  0
+   *  =        ~         ~    [    {      `       ||  \         }    ]      ,  .  /
+   *  shift/(  ctrl/esc  alt  gui  space  tab/L1  ||  enter/L2  bsp  altgr  -  '  shift/)
    */
-  [_RS] = LAYOUT(
-    KC_EXLM, KC_AT,   KC_UP,   KC_DLR,  KC_PERC,                  KC_PGUP, KC_7,    KC_8,   KC_9, KC_BSPC,
-    KC_LPRN, KC_LEFT, KC_DOWN, KC_RGHT, KC_RPRN,                  KC_PGDN, KC_4,    KC_5,   KC_6, KC_BSLS,
-    KC_LBRC, KC_RBRC, KC_HASH, KC_LCBR, KC_RCBR, KC_CIRC, KC_AMPR,KC_ASTR, KC_1,    KC_2,   KC_3, KC_PLUS,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,KC_SPC,  KC_TRNS, KC_DOT, KC_0, KC_EQL
+  [_NUM] = LAYOUT(
+    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_CIRC, KC_AMPR, KC_ASTR, KC_MINUS, KC_PLUS,
+    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,     KC_0,
+    KC_EQL,  KC_PIPE, KC_TILD, KC_LBRC, KC_LCBR, KC_GRV,    KC_BSLS, KC_RCBR, KC_RBRC, KC_COMM, KC_DOT,   KC_SLSH,
+    _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______, _______,
   ),
 
+  // TODO: browser back/forward macros (command + L/R arrow)
   /*
-   * insert  home   up     end    pgup        ||       up     F7  F8     F9      F10
-   * del     left   down   right  pgdn        ||       down   F4  F5     F6      F11
-   *         volup                reset       ||              F1  F2     F3      F12
-   *         voldn  super  shift  bksp  ctrl  ||  alt  space  L0  prtsc  scroll  pause
+   * F1       F2        F3      F4   F5             ||            F6       F7       F8       F9       F10
+   * cut      copy      insert  home pgup           ||             left     down     up       right    F11
+   * caps     paste     del     end  pgdn   undo    ||  again      mouse_l  mouse_d  mouse_u  mouse_r  prtscr
+   * shift/(  ctrl/esc  alt     gui  space  tab/L1  ||  enter/L2  bsp      altgr    -        '        shift/)
    */
-  [_LW] = LAYOUT(
-    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,   _______, _______, KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS
+  [_NAV] = LAYOUT(
+    KC_F1,   KC_F2,    KC_F3,  KC_F4,   KC_F5,                        KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,
+    KC_CUT,  KC_COPY,  KC_INS, KC_HOME, KC_PGUP,                      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_F11,
+    KC_CAPS, KC_PASTE, KC_DEL, KC_END,  KC_PGDN, KC_UNDO,    KC_AGIN, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,  KC_PSCR,
+    _______, _______,  _______, _______, _______, _______,   _______, _______, _______, _______, _______,
   )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case TMUX_LEAD:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTL(SS_TAP(X_B)));
+    }
+    break;
+  }
+  return true;
+}
